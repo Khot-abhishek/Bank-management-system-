cb37d1461b8d6514ece3194ce2143b91dc6d9d88
khot-abhishek <abhishekrkhot@gmail.com>
2023-03-16 20:00:34 +0530

    add the choice selection for banking operations, set some variables as GLOBAL

--- main.py
+++ main.py
@@ -27,8 +27,9 @@ from banking_operations import BankOperation
 from time import sleep
 
 # User constants
+RUNNING = True
 IS_AUTHENTICATED = False
-
+bank_operation_user = None
 
 def clear_screen():
 	os.system('cls')
@@ -37,19 +38,9 @@ def clear_screen():
 
 def main():
 	global IS_AUTHENTICATED
+	global bank_operation_user
+	global RUNNING
 
-	all_operations = {
-
-	'1': bank_operation_user.withdraw_amount,
-	'2': bank_operation_user.deposite_amount,
-	'3': bank_operation_user.show_balance,
-	'4': bank_operation_user.show_account_statement,
-	'5': bank_operation_user.change_pin,
-	'6': bank_operation_user.get_user_logged_out,
-	'7': bank_operation_user.end_program,
-}
-
-	
 	if IS_AUTHENTICATED:
 		logging.info('Displaying Banking OPtions after autherization ')
 		display_entry_window(BANKING_OPTIONS_LIST)
@@ -59,8 +50,23 @@ def main():
 			sleep(1)
 			clear_screen()
 		else:
-			operation = all_operations[choice]
-			operation()	
+			if choice == '1':
+				bank_operation_user.withdraw_amount()
+			elif choice == '2':
+				bank_operation_user.deposite_amount()
+			elif choice == '3':
+				logging.info(f'CHOICE-3: bank_operation_user: {bank_operation_user}')
+				bank_operation_user.show_balance()
+			elif choice == '4':
+				bank_operation_user.show_account_statement()
+			elif choice == '5':
+				bank_operation_user.change_pin()
+			elif choice == '6':
+				IS_AUTHENTICATED = False
+				bank_operation_user = None
+			elif choice == '7':
+				global RUNNING
+				RUNNING = False	
 	else:
 		display_window(ENTRY_OPTION_LIST)
 		print()
@@ -80,18 +86,15 @@ def main():
 				user_account = Account.objects.get(owner_id=user_object.id)
 				logging.info(f"Initializing the BANK-OPERATION-obj to carryout banking \noperations for current logged-in user")
 				bank_operation_user = BankOperation(user_object, user_account)
+				logging.info(f'bank_operation_user: {bank_operation_user}')
 
 		elif choice == '2':
 			user_signup()
 			clear_screen()
 		else:
-			global RUNNING
 			RUNNING = False
 			logging.info('Program Terminated')
 			
-
-
-RUNNING = True
 
 if __name__ == '__main__':
 	text = 'start of the program'.center(70, "-")
