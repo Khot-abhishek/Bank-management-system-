28d91c4877dffa2c205ae483ac3c25a1dad6a63a
khot-abhishek <abhishekrkhot@gmail.com>
2023-03-16 17:39:03 +0530

    initialize a BankOperation obj to carryout all banking operations
    also create a dict mapped to banking operation to their respectivs
    choices

--- main.py
+++ main.py
@@ -23,24 +23,44 @@ from bank_database.models import *
 
 from window_output import *
 from feature_functions import *
+from banking_operations import BankOperation
 from time import sleep
 
 # User constants
-USER_PROFILE = {}
 IS_AUTHENTICATED = False
 
 
 def clear_screen():
 	os.system('cls')
-	
+
+
 
 def main():
 	global IS_AUTHENTICATED
+
+	all_operations = {
+
+	'1': bank_operation_user.withdraw_amount,
+	'2': bank_operation_user.deposite_amount,
+	'3': bank_operation_user.show_balance,
+	'4': bank_operation_user.show_account_statement,
+	'5': bank_operation_user.change_pin,
+	'6': bank_operation_user.get_user_logged_out,
+	'7': bank_operation_user.end_program,
+}
+
 	
 	if IS_AUTHENTICATED:
 		logging.info('Displaying Banking OPtions after autherization ')
 		display_entry_window(BANKING_OPTIONS_LIST)
 		choice = input('\nEnter your choice: ')
+		if choice not in ['1','2','3','4','5','6','7']:
+			print('\nWRONG INPUT, please enter the correct Choice')
+			sleep(1)
+			clear_screen()
+		else:
+			operation = all_operations[choice]
+			operation()	
 	else:
 		display_window(ENTRY_OPTION_LIST)
 		print()
@@ -57,7 +77,10 @@ def main():
 			if login_response[0] == True:
 				IS_AUTHENTICATED = True
 				user_object = login_response[1]
-				
+				user_account = Account.objects.get(owner_id=user_object.id)
+				logging.info(f"Initializing the BANK-OPERATION-obj to carryout banking \noperations for current logged-in user")
+				bank_operation_user = BankOperation(user_object, user_account)
+				global bank_operation_user
 
 		elif choice == '2':
 			user_signup()
